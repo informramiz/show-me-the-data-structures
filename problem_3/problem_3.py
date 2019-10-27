9b2edcc4f28e3a44b9ba93f79e0650065e2c9ece
Ramiz Raja <informramiz@gmail.com>
2019-10-27 23:42:39 +0500

    feat: Write function to find letter codes

--- problem_3/problem_3.py
+++ problem_3/problem_3.py
@@ -61,6 +61,42 @@ def build_huffman_tree(char_count_dict):
     return queue.get()
 
 
+def __find_letter_codes(node, code_dict, code = ""):
+    """
+    Traverses the tree and builds the `code_dict` with letter codes
+    Args:
+        node(Node): Root node of huffman tree
+        code_dict(dict): Dictionary containing letter codes
+        code: code built till this moment/depth in a recursive order
+    Returns:
+        dict: A dictionary containing letter codes
+    """
+    if node is None:
+        return
+
+    if node.is_leaf_node():
+        code_dict[node.character] = code
+
+    __find_letter_codes(node.left, code_dict, code + "0")
+    __find_letter_codes(node.right, code_dict, code + "1")
+
+
+def find_letter_codes(node):
+    """
+    Traverses the tree and builds the `code_dict` with letter codes
+    Args:
+        node(Node): Root node of huffman tree
+    Returns:
+        dict: A dictionary containing letter codes
+    """
+    letter_codes_dict = {}
+    if node is None:
+        return letter_codes_dict
+
+    __find_letter_codes(node, letter_codes_dict)
+    return letter_codes_dict
+
+
 def huffman_encoding(text):
     if text is None or len(text) == 0:
         return None, None
@@ -70,7 +106,8 @@ def huffman_encoding(text):
         char_count_dict[char] = char_count_dict.get(char, 0) + 1
 
     tree = build_huffman_tree(char_count_dict)
-    print(tree)
+    letter_codes_dict = find_letter_codes(tree)
+    print(letter_codes_dict)
 
 def test():
     q = PriorityQueue()
