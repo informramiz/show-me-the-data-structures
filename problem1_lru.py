57e25f0d7e3a4560ebdd6f9ba78e0e86c61e5a65
Ramiz Raja <informramiz@gmail.com>
2019-10-27 18:33:28 +0500

    feat: Add test cases

--- problem1_lru.py
+++ problem1_lru.py
@@ -73,5 +73,40 @@ class LRU_Cache(object):
 
         self._items[key] = value
 
+    def __repr__(self):
+        return str(self._items)
+
+def assert_(expected, actual):
+    assert expected == actual, f"expected={expected}, actual={actual}"
+
+def test_cases():
+    our_cache = LRU_Cache(5)
+
+    our_cache.set(1, 1)
+    our_cache.set(2, 2)
+    our_cache.set(3, 3)
+    our_cache.set(4, 4)
+
+    output = our_cache.get(1)  # returns 1
+    assert_(1, output)
+
+    output = our_cache.get(2)  # returns 2
+    assert_(2, output)
+
+    output = our_cache.get(9)  # returns -1 because 9 is not present in the cache
+    assert_(-1, output)
+
+    our_cache.set(5, 5)
+    our_cache.set(6, 6)
+
+    output = our_cache.get(3)  # returns -1 because the cache reached it's capacity and 3 was the least recently used entry
+    assert_(-1, output)
+
+    # edge cases
+    our_cache.set(None, None)
+    our_cache.get(None)
+
+test_cases()
+
 
 
